a4f02e77729961da5470fe289c2adf64c4b519e9
pharouqy <pharouqy@gmail.com>
2023-05-05 08:24:51 +0100

    Project finaliser !!!

--- js/script.js
+++ js/script.js
@@ -8,10 +8,25 @@ const year = document.querySelector(
   "section:nth-child(1) > div:nth-child(3) > input"
 );
 
-const date = new Date();
+const button = document.getElementsByTagName("svg")[0];
 
-function dateRetrieve(day, month, year) {
-  let array = [];
+const resultYears = document.querySelector(
+  "section:nth-child(3) > div:nth-child(1) > span"
+);
+const resultMonths = document.querySelector(
+  "section:nth-child(3) > div:nth-child(2) > span"
+);
+const resultDays = document.querySelector(
+  "section:nth-child(3) > div:nth-child(3) > span"
+);
+
+const now = new Date();
+const annees = now.getFullYear();
+const mois = now.getMonth();
+const jours = now.getDate();
+
+async function handelDate(day, month, year) {
+  const array = [];
   day.addEventListener("change", async (e) => {
     const errorDay = document.getElementById("error-day");
     if (e.target.value != "" && e.target.value <= 31) {
@@ -38,17 +53,43 @@ function dateRetrieve(day, month, year) {
   });
   year.addEventListener("change", async (e) => {
     const errorYear = document.getElementById("error-year");
-    if (e.target.value != "" && e.target.value <= date.getFullYear()) {
+    if (e.target.value != "" && e.target.value <= now.getFullYear()) {
       value = await e.target.value;
       array.push(value);
       errorYear.textContent = "";
-    } else if (e.target.value >= date.getFullYear()) {
+    } else if (e.target.value >= now.getFullYear()) {
       errorYear.textContent = "Must be in the past";
     } else {
       errorYear.textContent = "This field is required";
     }
   });
-  return array;
 }
 
-const dateArray = dateRetrieve(day, month, year);
+handelDate(day, month, year);
+
+function calculateAge() {
+  button.addEventListener("click", (e) => {
+    let d = jours - day.value;
+    let m = mois - month.value;
+    let y = annees - year.value;
+    if (m < 0 || (m === 0 && jours < parseInt(day.value))) {
+      y--;
+      m += 12;
+    }
+    if (d < 0) {
+      m--;
+      d += 31;
+    }
+    if (m < 0) {
+      m = 11;
+      y--;
+    }
+    if (d != jours && m != month && y != annees) {
+      resultDays.textContent = d;
+      resultMonths.textContent = m;
+      resultYears.textContent = y;
+    }
+  });
+}
+
+calculateAge();
